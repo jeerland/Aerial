fbba8b18198e9d288dcda27dc4b40e4880a76e80
jeerland <jacco@eerland.net>
2016-02-09 08:28:29 +0100

    Added SharedCache and CacheDir properties

--- ScreenSaver/RegSettings.cs
+++ ScreenSaver/RegSettings.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
+using System;
+using System.IO;
 
 namespace ScreenSaver
 {
@@ -9,16 +11,20 @@ namespace ScreenSaver
         public bool UseTimeOfDay = true;
         public bool MultiscreenDisabled = true;
         public bool CacheVideos = false;
+        public bool SharedCache = false;
+        public string CacheDir = "";
 
         public RegSettings()
         {
             RegistryKey key = Registry.CurrentUser.OpenSubKey(keyAddress);
             if (key != null)
             {
-                DifferentMoviesOnDual = bool.Parse(key.GetValue(nameof(DifferentMoviesOnDual)) as string ?? "True");
-                UseTimeOfDay = bool.Parse(key.GetValue(nameof(UseTimeOfDay)) as string ?? "True");
-                MultiscreenDisabled = bool.Parse(key.GetValue(nameof(MultiscreenDisabled)) as string ?? "True");
-                CacheVideos = bool.Parse(key.GetValue(nameof(CacheVideos)) as string ?? "True");
+                DifferentMoviesOnDual = bool.Parse(key.GetValue("DifferentMoviesOnDual") as string ?? "True");
+                UseTimeOfDay = bool.Parse(key.GetValue("UseTimeOfDay") as string ?? "True");
+                MultiscreenDisabled = bool.Parse(key.GetValue("MultiscreenDisabled") as string ?? "True");
+                CacheVideos = bool.Parse(key.GetValue("CacheVideos") as string ?? "True");
+                SharedCache = CacheVideos && bool.Parse(key.GetValue("SharedCache") as string ?? "True");
+                CacheDir = key.GetValue("CacheDir") as string ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Aerial");
             }
         }
 
@@ -29,12 +35,12 @@ namespace ScreenSaver
         {
             RegistryKey key = Registry.CurrentUser.CreateSubKey(keyAddress);
             
-            key.SetValue(nameof(DifferentMoviesOnDual), DifferentMoviesOnDual);
-            key.SetValue(nameof(UseTimeOfDay), UseTimeOfDay);
-            key.SetValue(nameof(MultiscreenDisabled), MultiscreenDisabled);
-            key.SetValue(nameof(CacheVideos), CacheVideos);
-
+            key.SetValue("DifferentMoviesOnDual", DifferentMoviesOnDual);
+            key.SetValue("UseTimeOfDay", UseTimeOfDay);
+            key.SetValue("MultiscreenDisabled", MultiscreenDisabled);
+            key.SetValue("CacheVideos", CacheVideos);
+            key.SetValue("SharedCache", SharedCache);
+            key.SetValue("CacheDir", CacheDir);
         }
-
     }
 }
